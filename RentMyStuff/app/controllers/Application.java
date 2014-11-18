9f05898e0cdf1664d292770ff88a68c08812df7c
jagaidec <jagaidec@htwg-konstanz.de>
2014-11-18 21:25:16 +0100

    CSS bug fixes and Play changes

--- RentMyStuff/app/controllers/Application.java
+++ RentMyStuff/app/controllers/Application.java
@@ -7,8 +7,35 @@ import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
+	public static Result index() {
+        return ok(index.render());
     }
+	
+	public static Result login(){
+		return ok(login.render());
+	}
+
+	public static Result angebote(){
+		return ok(angebote.render());
+	}
+	public static Result gesuche(){
+		return ok(gesuche.render());
+	}
+	
+	public static Result kontakt(){
+		return ok(kontakt.render());
+	}
+	
+	public static Result impressum(){
+		return ok(impressum.render());
+	}
+	
+	public static Result inserat(){
+		return ok(inserat.render());
+	}
+	
+	public static Result registrieren(){
+		return ok(registrieren.render());
+	}
 
 }
