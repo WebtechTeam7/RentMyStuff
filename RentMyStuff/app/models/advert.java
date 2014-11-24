d47c07f77b92c8de601160457c915764cf6ae082
Dennis Klein <Dennis.Jens.Klein@gmail.com>
2014-11-24 15:01:42 +0100

    Getter und Setter hinzugefuegt

--- RentMyStuff/app/models/advert.java
+++ RentMyStuff/app/models/advert.java
@@ -14,6 +14,31 @@ public class advert {
 	private String description;
 	private boolean file_existing;
 	
+	public String getKind() {
+		return kind;
+	}
+	public void setKind(String kind) {
+		this.kind = kind;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public boolean isFile_existing() {
+		return file_existing;
+	}
+	public void setFile_existing(boolean file_existing) {
+		this.file_existing = file_existing;
+	}
+	
 	
 	
 }
