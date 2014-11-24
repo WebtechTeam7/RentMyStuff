01db5be5b5ab016ab60a9af189ff00155c73c99d
Dennis Klein <Dennis.Jens.Klein@gmail.com>
2014-11-24 14:57:33 +0100

    user und advert klassen hinzugefuegt

---
+++ RentMyStuff/app/models/advert.java
@@ -0,0 +1,19 @@
+package models;
+
+import play.*;
+
+
+public class advert {
+
+	public advert(){
+		
+	}
+	
+	private String kind;
+	private	String category;
+	private String description;
+	private boolean file_existing;
+	
+	
+	
+}
---
+++ RentMyStuff/app/models/user.java
@@ -0,0 +1,19 @@
+package models;
+
+import play.*;
+
+
+public class user {
+	
+	public user{
+		
+	}
+	
+	private String firstname;
+	private String lastname;
+	private String email;
+	private String password;
+	
+	
+
+}
