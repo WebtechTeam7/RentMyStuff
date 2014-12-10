00a40df773c889abd87c3e577742ebeec43d5186
jagaidec <jagaidec@htwg-konstanz.de>
2014-12-10 17:48:47 +0100

    Datenbank Schema ueberarbeiter, User via Script angelegt

---
+++ RentMyStuff/app/db/INSERT_USER_RENTMYSTUFF.sql
@@ -0,0 +1,8 @@
+INSERT INTO USER (Firstname, Lastname, Email, Password)
+VALUES ('Jan', 'Gaideczka', 'jan.gaideczka@htwg-konstanz.de', 'Geheim123!');
+
+INSERT INTO USER (Firstname, Lastname, Email, Password)
+VALUES ('Dennis', 'Klein', 'dennis.klein@htwg-konstanz.de', 'Geheim123!');
+
+INSERT INTO USER (Firstname, Lastname, Email, Password)
+VALUES ('Ramona', 'Barth', 'ramona.barth@htwg-konstanz.de', 'Geheim123!');
