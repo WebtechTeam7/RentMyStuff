ffff6a71b8711e07e3c09f4a83d9b86dead80772
Dennis Klein <Dennis.Jens.Klein@gmail.com>
2015-01-18 15:58:30 +0100

    Code entruempelt

--- public/javascripts/ajax.js
+++ public/javascripts/ajax.js
@@ -1,32 +1,14 @@
-// Eigene Angebote und Gesuche
-
-// Variante mit AJAX Jquery
+// 1. Variante mit AJAX Jquery
 
 /*function reloadAdverts(){
 	$(document).ready(function(){
-		  $("btAngebote").click(function(){
+		  $("Angebote").click(function(){
 		    $("#Angebote").load("getAdvertList");
 		   });
 		});
 }
 
-/*function ownSearchTest(){
-	$(document).ready(function(){
-		  $("btAngebote").click(function(){
-		    $("#Angebote").load("getAdvertList");
-		   });
-		});
-}
-
-
-/*function ownAdvert2(){
-	$(document).ready(function(){
-		  $("btAngebote").click(function(){
-		    $("#Angebote").load("@routes.Assets.at("app/views/ajaxEigeneAngebote.html")");
-		  });
-		});
-}*/
-
+*/
 // 2. Variante mit HTML
 
 function reloadAdverts() {
